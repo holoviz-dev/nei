13005f26432731fb393cfd71aa577b44d12b935f
jlstevens <jlstevens@ed.ac.uk>
2018-02-11 01:14:32 +0000

    Added examples/demo.py

---
+++ examples/demo.py
@@ -0,0 +1,25 @@
+# -*- mode: python; labmode-env : "analytics"; eval: (lab-mode)-*-
+
+"Code before the first cell is ignored."
+
+# In[ ]
+print("This is an example of code execution: %s is %d" % ("1+1", 1+1))
+
+
+"""
+### Markdown is supported and live!!
+
+* Lists are live
+* **bold**
+* *italic*
+"""
+
+# In[ ]
+import holoviews as hv
+import numpy as np
+hv.extension('bokeh')
+
+
+# In[ ]
+xs = np.linspace(1,10,100)
+hv.Curve((xs, np.sin(xs)))
