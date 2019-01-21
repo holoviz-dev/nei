9c81908aca1cbc4a2facddfd136c1aec65e903d1
jlstevens <jlrstevens@gmail.com>
2019-01-21 01:32:54 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup
 
+install_requires = [
+    'jupyter_client',
+    'nbformat',
+    'tornado==5.1.1',
+]
+
+extras_require = {
+    'pyviz': [
+        'bokeh',
+        'holoviews',
+        'cssutils',
+        'nbconvert'
+    ]
+}
+
 setup_args = dict(
     name='nei',
     version='0.0.3',
-    description='Notebook Emacs Integration',
+    description='Notebook Emacs Interface',
     platforms=['Windows', 'Mac OS X', 'Linux'],
     license='BSD',
     provides=["nei"],
     include_package_data = True,
-    python_requires=">=3.5")
+    python_requires=">=3.5",
+    install_requires=install_requires,
+    extras_require=extras_require)
 
 if __name__=="__main__":
     setup(**setup_args)
