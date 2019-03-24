9ed8146c6f545bb02620d5b47dbab694f207f05e
jlstevens <jlrstevens@gmail.com>
2019-03-23 22:13:42 -0500

    Added integration test that executes a cell

--- tests/integration-tests.el
+++ tests/integration-tests.el
@@ -11,6 +11,12 @@
 
 
 (defun nei--proxy-browser ()
+  (if (null nei--ws-proxy-browser-connection)
+      (nei--connect-proxy-browser))
+  )
+
+
+(defun nei--connect-proxy-browser ()
   (progn
     (setq conn (websocket-open
                 "ws://127.0.0.1:10000"
@@ -26,35 +32,36 @@
     )
   )
 
-
-(defun assert-response-properties (cmd &optional args)
+(defmacro assert-response-properties (cmd block);; &optional args)
   "Given the response JSON assert the command matches and optionally the args"
+  `(progn
+     (check-websocket-connections)
+     (setq nei--test-response nil)
 
-  (let
-      ((result (wait-for-response)))
-    (should (equal (null result) nil)) 
-    (should (equal (assoc-value 'cmd result) cmd))
-    )
+     (let ((i 0))
+       ,block
+       (while (and (null nei--test-response) (< i 350))
+         (sleep-for 0 10)
+         (setq i (+ i 1))
+         )
+       (should (equal (null nei--test-response) nil))
+       (should (equal
+                (assoc-value 'cmd (json-read-from-string nei--test-response)) ,cmd))
+       )
+     )
   )
 
 
-(defun wait-for-response ()
-  (let ((result nil) (i 0))
-    (while (and (null nei--test-response) (< i 150))
-      (sleep-for 0 10)
-      (setq i (+ i 1))
-      )
-    (setq result nei--test-response)
-    (setq nei--test-response nil)
-    (if (null result) result (json-read-from-string result)) 
-    )
+(defun check-websocket-connections ()
+  (should (equal
+           (websocket-openp nei--ws-proxy-browser-connection) '(open run)))
+  (should (equal
+           (websocket-openp nei--ws-connection) '(open run)))
   )
-
 
 (ert-deftest test-server-port-available ()
   (should (equal (nei--server-available 8001) t))
   )
-
 
 (ert-deftest test-update-css ()
   (nei-connect 10000 8010 t)
@@ -68,7 +75,54 @@
     (should (equal (null nei--ws-proxy-browser-connection) nil))
     (nei-mode)
     (should (equal nei--currently-mirroring t))
-    (nei-update-css)
-    (assert-response-properties "update_style")
+    (assert-response-properties "update_style" (nei-update-css))
+    )
+  )
+
+(ert-deftest test-simple-insert-code-cell ()
+  (nei-connect 10000 8010 t)
+  (should (equal nil (null (get-process "nei-server"))))
+  (sleep-for 0.2)
+
+  (with-temp-buffer
+    (setq nei-autoconnect nil)
+    (setq python-indent-guess-indent-offset nil)
+    (nei--proxy-browser)
+    (should (equal (null nei--ws-proxy-browser-connection) nil))
+    (nei-mode)
+    (should (equal nei--currently-mirroring t))
+
+    (assert-response-properties "add_cell"
+                                (progn 
+                                  (insert "Text before any cell is not part of the notebook\n")
+                                  (insert "# In[ ]\n\"But this is now a string in a code cell\"")
+                                )
+    )
+    )
+  )
+
+(ert-deftest test-simple-insert-code-cell-and-execute ()
+  (nei-connect 10000 8010 t)
+  (should (equal nil (null (get-process "nei-server"))))
+  (sleep-for 0.2)
+
+  (with-temp-buffer
+    (setq nei-autoconnect nil)
+    (setq python-indent-guess-indent-offset nil)
+    (nei--proxy-browser)
+    (should (equal (null nei--ws-proxy-browser-connection) nil))
+    (nei-mode)
+    (should (equal nei--currently-mirroring t))
+    (assert-response-properties "add_cell"
+                                (progn
+                                  (insert "Text before any cell is not part of the notebook\n")
+                                  (insert "# In[ ]\n\"But this is now a string in a code cell\"")
+      )
+    )
+    (nei-start-kernel)
+    (assert-response-properties "update_cell_input"
+                                (progn
+                                  (goto-char (point-max))
+                                  (nei-exec-by-line)))
     )
   )
