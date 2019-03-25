321c7d1d836d9ee71f82448df4f0cf781a270bec
jlstevens <jlrstevens@gmail.com>
2019-03-25 00:23:55 -0500

    Updated the datashader_ny.py example

--- examples/datashader_ny.py
+++ examples/datashader_ny.py
@@ -1,6 +1,7 @@
 
 # In[ ]
 import holoviews as hv, datashader as ds
+from holoviews import opts
 import dask.dataframe as dd
 from holoviews.operation.datashader import rasterize
 
@@ -10,12 +11,14 @@ hv.extension('bokeh')
 path = '../../../datashader/examples/data/nyc_taxi_wide.parq'
 df = dd.read_parquet(path).persist()
 
-opts = dict(width=700, height=300, logz=True, tools=['hover'], colorbar=True)
 points = hv.Points(df, ['dropoff_x', 'dropoff_y'], ['dropoff_hour','pickup_hour'])
 
 
 # In[ ]
-rasterize(points).options(**opts)
+options = opts.Image( data_aspect=1, responsive=True, logz=True,
+                      tools=['hover'], colorbar=True)
+rasterize(points).options(options)
+
 
 # In[ ]
 import panel
