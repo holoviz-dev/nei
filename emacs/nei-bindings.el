fb648585540bbd8a62de681caba50e8580d0eb2c
jlstevens <jlrstevens@gmail.com>
2019-06-29 10:22:39 -0500

    Defined easier keybindings for navigating code and markdown cells

--- emacs/nei-bindings.el
+++ emacs/nei-bindings.el
@@ -39,10 +39,10 @@
   (define-key map (kbd "C-c h") 'nei-toggle-display-code)
   (define-key map (kbd "C-c H") 'nei-toggle-display-all-code)
 
-  (define-key map (kbd "C-c '") 'nei-move-point-to-next-code-cell)
-  (define-key map (kbd "C-c ;") 'nei-move-point-to-previous-code-cell)
-  (define-key map (kbd "C-c @") 'nei-move-point-to-next-markdown-cell)
-  (define-key map (kbd "C-c :") 'nei-move-point-to-previous-markdown-cell)
+  (define-key map (kbd "C-c C-'") 'nei-move-point-to-next-code-cell)
+  (define-key map (kbd "C-c C-;") 'nei-move-point-to-previous-code-cell)
+  (define-key map (kbd "C-c '") 'nei-move-point-to-next-markdown-cell)
+  (define-key map (kbd "C-c ;") 'nei-move-point-to-previous-markdown-cell)
   
   ;; More unusual bindings
   (define-key map [(S-return)] 'nei-exec-by-line-and-move-to-next-cell)
